ed588911ee39621501a8dad1187a7c7fba051d6c
inux <inux.steve@gmail.com>
2019-06-15 00:12:15 +0200

    rust fix

--- rust/tutorial/variables/src/main.rs
+++ rust/tutorial/variables/src/main.rs
@@ -19,4 +19,8 @@ fn main() {
     let six_point_four = x.1;
 
     let one = x.2;
+
+    println!("The value of five_hundred is: {}", five_hundred);
+    println!("The value of six_point_four is: {}", six_point_four);
+    println!("The value of one is: {}", one);
 }
